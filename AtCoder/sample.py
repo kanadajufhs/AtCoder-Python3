8d564211bfc008026e5997cb1a849fbae6488327
金田哲 <kanadajufhs@kndtkb.local>
2020-01-29 00:50:22 +0900

    add sample

---
+++ AtCoder/sample.py
@@ -0,0 +1,19 @@
+# 入力まとめ
+
+# H A
+H,N = map(int, input().split())
+
+# H N
+# A1 A2 ..An
+H,N = map(int, input().split())
+A = list(map(int, input().split())
+
+# H N
+# A1 B1
+# ...
+# An Bn
+H,N = map(int, input().split())
+for i in range(N):
+    A,B = map(int,input().split())
+    a.append(A)
+    b.append(B)
